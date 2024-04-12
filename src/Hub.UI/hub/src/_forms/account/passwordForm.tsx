5fc794303542a198a0504d443e57b2e0a7a6aa53
George Maia <georgemaiaf@gmail.com>
2024-04-11 23:40:17 -0300

    feat(Table): :triangular_flag_on_post: Accounts

--- src/Hub.UI/hub/src/_forms/account/passwordForm.tsx
+++ src/Hub.UI/hub/src/_forms/account/passwordForm.tsx
@@ -71,4 +71,4 @@ export const UpdatePasswordForm = () => {
   );
 
   return <Form />;
-}
+};
--- src/Hub.UI/hub/src/_tables/accountsTable.tsx
+++ src/Hub.UI/hub/src/_tables/accountsTable.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { MaterialReactTable, MRT_ColumnFiltersState, MRT_PaginationState, MRT_TableInstance, useMaterialReactTable, type MRT_ColumnDef, type MRT_SortingState } from "material-react-table";
-import { Send } from "@mui/icons-material";
+import { jsPDF } from "jspdf";
+import autoTable from "jspdf-autotable";
+import { MaterialReactTable, MRT_Cell, MRT_ColumnFiltersState, MRT_PaginationState, MRT_Row, MRT_ShowHideColumnsButton, MRT_TableInstance, MRT_ToggleDensePaddingButton, MRT_ToggleFiltersButton, MRT_ToggleFullScreenButton, MRT_ToggleGlobalFilterButton, useMaterialReactTable, type MRT_ColumnDef, type MRT_SortingState } from "material-react-table";
+import { Add, ClearAll, Delete, Edit, FileDownload, Share } from "@mui/icons-material";
 import { Box, Button, IconButton } from "@mui/material";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useContext, useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import env from "@/env";
@@ -11,6 +13,8 @@ import { selectStatus, selectTotalCount, selectUsers } from "@/_redux/features/u
 import { getUsers } from "@/_redux/features/user/thunks";
 import { AppDispatch } from "@/_redux/store";
 import { user, userFilter } from "@/_types";
+import { useSnackbar } from "@/_contexts";
+import { colors, ThemeContext } from "@/_theme";
 
 export const AccountsTable = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -21,9 +25,9 @@ export const AccountsTable = () => {
   const status = useSelector(selectStatus);
   const isLoading = () => status === "idle" || status === "loading";
 
-  let params: URLSearchParams;
-  if (typeof window === "undefined") params = new URLSearchParams();
-  else params = new URLSearchParams(window.location.search);
+  const params: URLSearchParams = typeof window === "undefined" ?
+    new URLSearchParams() :
+    new URLSearchParams(window.location.search);
 
   const [globalFilter, setGlobalFilter] = useState<string>(params.get("search") ?? "");
   const [sorting, setSorting] = useState<MRT_SortingState>([]);
@@ -32,6 +36,9 @@ export const AccountsTable = () => {
     pageIndex: parseInt(params.get("pageIndex") as string) || 0,
     pageSize: parseInt(params.get("pageSize") as string) || 10
   });
+
+  const snackbar = useSnackbar();
+  const { themeName } = useContext(ThemeContext);
 
   useEffect(() => { onsubmit(); }, []);
 
@@ -43,6 +50,8 @@ export const AccountsTable = () => {
   // useEffect(() => { if (env.NODE_ENV !== "production") console.debug("rowCount", rowCount); }), [rowCount];
 
   const buildQueryString = useCallback(() => {
+    Array.from(params.keys()).forEach(key => params.delete(key));
+
     globalFilter && params.set("search", globalFilter);
     sorting.forEach(sort => params.set(sort.id + "Sort", sort.desc ? "desc" : "asc"));
     columnFilters.forEach(filter => params.set(filter.id, filter.value as string));
@@ -87,70 +96,97 @@ export const AccountsTable = () => {
     window.history.replaceState({}, "", newUrl);
   }, [status]);
 
-  const columns = useMemo<MRT_ColumnDef<user>[]>(
-    () => [
-      {
-        accessorKey: "id",
-        header: "ID",
-        size: 50,
-        Cell: ({ row }) => row.original.id ? row.original.id.slice(0, 8) + "..." : "",
-        enableSorting: false,
-      },
-      {
-        accessorKey: "userName",
-        header: "User Name",
-        size: 100,
-      },
-      {
-        accessorKey: "email",
-        header: "Email",
-        size: 150,
-      },
-      {
-        accessorKey: "phoneNumber",
-        header: "Phone Number",
-        size: 100,
-        enableSorting: false,
-      },
-      {
-        accessorKey: "roles",
-        header: "Roles",
-        size: 100,
-        Cell: ({ row }) => row.original.roles.join(", "),
-        enableSorting: false,
-      },
-    ],
-    [],
+  const handleShare = () => {
+    const url = typeof window === "undefined" ? "" : `${window.location.href}`;
+
+    navigator.clipboard.writeText(url)
+      .then(() => snackbar("Link copied to clipboard"));
+
+    if (env.NODE_ENV !== "production")
+      console.debug("handleShare", url);
+  };
+
+  const handleClearFilters = () => {
+    setGlobalFilter("");
+    setSorting([]);
+    setColumnFilters([]);
+    setPagination({ pageIndex: 0, pageSize: 10 });
+
+    Array.from(params.keys()).forEach(key => params.delete(key));
+
+    onsubmit();
+  };
+
+  const handleExportRows = (rows: MRT_Row<user>[]) => {
+    const doc = new jsPDF();
+    const tableData = rows.map(row => Object.values(row.original));
+    const tableHeaders = columns.map(c => c.header);
+
+    autoTable(doc, {
+      head: [tableHeaders],
+      body: tableData,
+    });
+
+    doc.save(`accounts-${new Date().toISOString()}.pdf`);
+  };
+
+  const columns = useMemo<MRT_ColumnDef<user>[]>(() => [
+    {
+      accessorKey: "id",
+      header: "ID",
+      size: 80,
+      Cell: ({ row }) => row.original.id ? row.original.id.slice(0, 8) + "..." : "",
+      enableSorting: false,
+    },
+    {
+      accessorKey: "userName",
+      header: "User Name",
+      size: 150,
+    },
+    {
+      accessorKey: "email",
+      header: "Email",
+      size: 250,
+    },
+    {
+      accessorKey: "phoneNumber",
+      header: "Phone Number",
+      size: 200,
+      enableSorting: false,
+    },
+    {
+      accessorKey: "roles",
+      header: "Roles",
+      size: 200,
+      Cell: ({ row }) => row.original.roles.join(", "),
+      enableSorting: false,
+    },
+  ], [],);
+
+  const renderRowActions = ({ cell, row, table }: { cell: MRT_Cell<user>, row: MRT_Row<user>, staticRowIndex?: number | undefined, table: MRT_TableInstance<user> }) => [
+    <IconButton key="edit" size="small" onClick={() => console.debug(row.original.id)}><Edit /></IconButton>,
+    <IconButton key="delete" size="small" onClick={() => console.debug(row.original.id)}><Delete /></IconButton>,
+  ];
+
+  const renderTopToolbarCustomActions = ({ table }: { table: MRT_TableInstance<user> }) => (
+    <Box display="flex" alignItems="center" gap={1}>
+      <Button key="run" variant="contained" size="small" onClick={onsubmit}>Run</Button>
+      <Button key="create" variant="outlined" size="small" endIcon={<Add />} onClick={() => console.debug("create")}>Create</Button>
+      <Button key="edit" variant="outlined" size="small" endIcon={<Edit />} onClick={() => console.debug("edit")}>Edit</Button>
+      <Button key="delete" variant="outlined" size="small" endIcon={<Delete />} onClick={() => console.debug("delete")}>Delete</Button>
+    </Box>
   );
 
-  const renderTopToolbarCustomActions = ({ table }: { table: MRT_TableInstance<user> }) => {
-    const Buttons = () => (
-      <>
-        <Button
-          variant="contained"
-          size="small"
-          onClick={onsubmit}
-        >
-          Run
-        </Button>
-      </>
-    );
-
-    const IconButtons = () => (
-      <>
-        <IconButton size="small">
-          <Send fontSize="inherit" />
-        </IconButton>
-      </>
-    );
-
-    return (
-      <Box display='flex' justifyContent='space-between' width='-webkit-fill-available' height={40}>
-        <Buttons />
-        <IconButtons />
-      </Box>
-    );
-  };
+  const renderToolbarInternalActions = ({ table }: { table: MRT_TableInstance<user> }) => [
+    <MRT_ToggleGlobalFilterButton key="globalFilter" table={table} />,
+    <IconButton key="share" size="medium" aria-label="teste" onClick={handleShare}><Share /></IconButton>,
+    <IconButton key="clear-filters" size="medium" onClick={handleClearFilters}><ClearAll /></IconButton>,
+    <IconButton key="export" size="medium" onClick={() => handleExportRows(table.getRowModel().rows)}><FileDownload /></IconButton>,
+    <MRT_ToggleFiltersButton key="toggleFilters" table={table} />,
+    <MRT_ShowHideColumnsButton key="showHideColumns" table={table} />,
+    <MRT_ToggleDensePaddingButton key="toggleDensePadding" table={table} />,
+    <MRT_ToggleFullScreenButton key="toggleFullScreen" table={table} />,
+  ];
 
   const table = useMaterialReactTable({
     columns,
@@ -162,6 +198,39 @@ export const AccountsTable = () => {
       },
     }),
 
+    muiTopToolbarProps: {
+      sx: () => ({
+        "& .MuiIconButton-root": {
+          color: `${themeName === "light" ? colors.black : colors.white}`
+        },
+      })
+    },
+
+    muiTableProps: {
+      sx: () => ({
+        "& .MuiIconButton-root, .MuiSelect-icon": {
+          color: `${themeName === "light" ? colors.black : colors.white}`
+        },
+      })
+    },
+
+    muiFilterSliderProps: {
+      sx: () => ({
+        "& .MuiIconButton-root, .MuiSelect-icon": {
+          color: `${themeName === "light" ? colors.black : colors.white}`
+        },
+      })
+    },
+
+    muiBottomToolbarProps: {
+      sx: () => ({
+        "& .MuiIconButton-root, .MuiSelect-icon": {
+          color: `${themeName === "light" ? colors.black : colors.white}`
+        },
+      })
+    },
+
+    renderToolbarInternalActions: renderToolbarInternalActions,
     renderTopToolbarCustomActions: renderTopToolbarCustomActions,
 
     initialState: {
@@ -185,6 +254,12 @@ export const AccountsTable = () => {
     onPaginationChange: setPagination,
 
     rowCount: rowCount ?? 0,
+
+    // enableRowActions: true,
+    // renderRowActions: renderRowActions,
+
+    enableRowSelection: true,
+    getRowId: row => row.id ?? "",
 
     state: {
       isLoading: isLoading(),
